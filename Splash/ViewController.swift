2fedcc0ddc07b2d2bb8c63026132f8a4303c9e4b
Dima <dimazen.dev@gmail.com>
2019-01-27 18:25:57 +0200

    Init

---
+++ Splash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Splash
+//
+//  Created by Dima Vorona on 27/01/2019.
+//  Copyright © 2019 Dima Vorona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
